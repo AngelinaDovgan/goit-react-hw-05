a266bd824882d2d1c1e0b522191b4609e8313e7b
Angelina <midnightshout03@gmail.com>
2024-04-26 16:49:30 +0300

    15

--- src/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -61,9 +61,10 @@ export default function MovieDetailsPage() {
 
     const defaultImg = 'https://dl-media.viber.com/10/share/2/long/vibes/icon/image/0x0/95e0/5688fdffb84ff8bed4240bcf3ec5ac81ce591d9fa9558a3a968c630eaba195e0.jpg';
 
+   
+
     return (
         <>
-            
             <BackLink to={backLinkHref.current}></BackLink>
             
             <main>
@@ -75,6 +76,7 @@ export default function MovieDetailsPage() {
                             (`https://image.tmdb.org/t/p/w500/${poster_path}`) :
                             defaultImg
                     } alt={title} />
+                   
                 </div>
                 <div>
                     <h2>{title}</h2>
